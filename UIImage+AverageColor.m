e8d6ac329bc29aa7bf02f24e8ef27e5aed8224ac
Elijah Frederickson <elijah.frederickson@gmail.com>
2014-07-02 14:07:12 -0800

    Fix/update Adaptive colirng

--- UIImage+AverageColor.m
+++ UIImage+AverageColor.m
@@ -1,6 +1,9 @@
 #import <CoreGraphics/CoreGraphics.h>
 #import <UIKit/UIColor.h>
 #import "UIImage+AverageColor.h"
+#import <QuartzCore/QuartzCore.h>
+#import <UIKit/UIKit.h>
+
 @implementation UIImage (UIImageAverageColorAddition)
 /*- (UIColor *)averageColor
 {
@@ -36,6 +39,7 @@
 	return [UIColor colorWithRed:f * red  green:f * green blue:f * blue alpha:1];
 }*/
 
+
 - (UIColor *)averageColor {
     CGSize size = {1, 1};
     UIGraphicsBeginImageContext(size);
@@ -43,8 +47,36 @@
     CGContextSetInterpolationQuality(ctx, kCGInterpolationMedium);
     [self drawInRect:(CGRect){.size = size} blendMode:kCGBlendModeCopy alpha:1];
     uint8_t *data = CGBitmapContextGetData(ctx);
-    UIColor *color = [UIColor colorWithRed:data[0] / 255.f green:data[1] / 255.f blue:data[2] / 255.f alpha:1];
+    UIColor *color = [UIColor colorWithRed:data[1] / 255.f green:data[1] / 255.f blue:data[0] / 255.f alpha:1];
     UIGraphicsEndImageContext();
     return color;
 }
+
+
+/*
+- (UIColor *)averageColor {
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    unsigned char rgba[4];
+    CGContextRef context = CGBitmapContextCreate(rgba, 1, 1, 8, 4, colorSpace, kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+    
+    CGContextDrawImage(context, CGRectMake(0, 0, 1, 1), self.CGImage);
+    CGColorSpaceRelease(colorSpace);
+    CGContextRelease(context);
+    
+    if(rgba[3] > 0) {
+        CGFloat alpha = ((CGFloat)rgba[3])/255.0;
+        CGFloat multiplier = alpha/255.0;
+        return [UIColor colorWithRed:((CGFloat)rgba[0])*multiplier
+                               green:((CGFloat)rgba[1])*multiplier
+                                blue:((CGFloat)rgba[2])*multiplier
+                               alpha:alpha];
+    }
+    else {
+        return [UIColor colorWithRed:((CGFloat)rgba[0])/255.0
+                               green:((CGFloat)rgba[1])/255.0
+                                blue:((CGFloat)rgba[2])/255.0
+                               alpha:((CGFloat)rgba[3])/255.0];
+    }
+}*/
 @end
