9bd03d83bf2e0d8e5199a848cd2f2376589bc9cf
Elijah Frederickson <elijah.frederickson@gmail.com>
2014-07-02 13:43:48 -0800

    README

--- UIImage+AverageColor.m
+++ UIImage+AverageColor.m
@@ -2,7 +2,7 @@
 #import <UIKit/UIColor.h>
 #import "UIImage+AverageColor.h"
 @implementation UIImage (UIImageAverageColorAddition)
-- (UIColor *)averageColor
+/*- (UIColor *)averageColor
 {
     CGImageRef rawImageRef = [self CGImage];
     
@@ -34,5 +34,17 @@
     
 	CGFloat f = 1.0f / (255.0f * imageWidth * imageHeight);
 	return [UIColor colorWithRed:f * red  green:f * green blue:f * blue alpha:1];
+}*/
+
+- (UIColor *)averageColor {
+    CGSize size = {1, 1};
+    UIGraphicsBeginImageContext(size);
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    CGContextSetInterpolationQuality(ctx, kCGInterpolationMedium);
+    [self drawInRect:(CGRect){.size = size} blendMode:kCGBlendModeCopy alpha:1];
+    uint8_t *data = CGBitmapContextGetData(ctx);
+    UIColor *color = [UIColor colorWithRed:data[0] / 255.f green:data[1] / 255.f blue:data[2] / 255.f alpha:1];
+    UIGraphicsEndImageContext();
+    return color;
 }
 @end
